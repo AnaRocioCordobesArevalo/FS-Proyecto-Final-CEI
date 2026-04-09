1d15bcf228c9c8aa8125ba4b77c5b553d2567b3d
AnaRocioCordobesArevalo <annarca.6@gmail.com>
2026-04-09 17:33:31 +0200

    Modificaciones de los CRDUs

--- src/models/Books.js
+++ src/models/Books.js
@@ -1,19 +1,25 @@
-//conexión con el mongoose
+//Importamos mongoose
 import mongoose from "mongoose";
-//Tabla de la base de datos
+//Estructura de la base de datos
 const booksSchema = new mongoose.Schema({
     tittle: {
         type: String,
-        required: true
+        required: [true, "El título es obligatorio"],
+        minlength: [2, "El título debe tener mínimo 2 caracteres"],
+        maxlength: [100, "El título no puede superar 100 caracteres"],
+        trim: true
     },
     category: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Category",
-        required: true
+        required: [true, "La categoría es obligatoria"]
     },
     author: {
         type: String,
-        required: true
+        required: [true, "El autor es obligatorio"],
+        minlength: [3, "El autor debe tener mínimo 3 caracteres"],
+        maxlength: [50, "El autor no puede superar 50 caracteres"],
+        trim: true
     }
 });
 //Tabla donde te lo guarda  o que crea el modelo
--- src/models/Users.js
+++ src/models/Users.js
@@ -1,27 +1,35 @@
-//importamos mongoose para que se conecte con el modelo
+//Importamos mongoose
 import mongoose from "mongoose";
-//Como vamos a estructurar la base de datos, es decir, el modelo
+//Estructura de la base de datos
 const usersSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, "El nombre es obligatorio"],
+        minlength: [3, "El nombre debe tener mínimo 3 caracteres"],
+        maxlength: [50, "El nombre no puede superar 50 caracteres"],
+        trim: true // elimina espacios al inicio y al final
     },
-    email:{
-        type:String,
-        required: true,
-        unique: true
+    email: {
+        type: String,
+        required: [true, "El email es obligatorio"],
+        unique: true,
+        trim: true,
+        lowercase: true, // convierte a minúsculas automáticamente
+        match: [
+            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, //Esto me ha ayudado la IA
+            "El email no tiene un formato válido"
+        ]
     },
     password: {
-        type:String,
-        required: true,
-        unique: true
+        type: String,
+        required: [true, "La contraseña es obligatoria"],
+        minlength: [8, "La contraseña debe tener mínimo 8 caracteres"],
     },
     is_admin: {
         type: Boolean,
         required: true,
         default: false
-        
     }
 });
 //Tabla donde te lo guarda  o que crea el modelo
-export default mongoose.models.Users || mongoose.model ("Users", usersSchema);
+export default mongoose.models.Users || mongoose.model("Users", usersSchema);
